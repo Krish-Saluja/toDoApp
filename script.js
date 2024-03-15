3e5333872eac3c123e75245362741ca95dd17ba8
Krish Saluja <krishsaluja297@gmail.com>
2024-03-15 16:16:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function addToDo() {
+    const title = document.getElementById("title").value;
+    const desc = document.getElementById("desc").value;
+    console.log(title);
+    console.log(desc);
+}
