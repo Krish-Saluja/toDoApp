db98b5f57d7a7b45a4e3a8f6bb01129dfcebdcef
Krish Saluja <krishsaluja297@gmail.com>
2024-03-15 17:00:59 +0530

    Made the site interactive, added a button that checks off the task if you click it

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function addToDo() {
     const title = document.getElementById("title").value;
     const desc = document.getElementById("desc").value;
-    console.log(title);
-    console.log(desc);
+    const newToDo = document.createElement("div");
+
+    newToDo.innerHTML = `<h4>${title}</h4> <p>${desc}</p> <button class="markAsDone">Mark as done</button>` 
+
+    const toDoList =  document.getElementById("add");
+    toDoList.appendChild(newToDo);
+    
+    const btn = newToDo.querySelector('.markAsDone')
+    btn.addEventListener('click', function(){
+        newToDo.innerHTML=`<s><h4>${title}</h4> <p>${desc}</p></s>`
+    })
 }
